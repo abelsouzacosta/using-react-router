6d02caccf28a0a54f61267a662f2e059bb958335
Abel Souza <abelsouzacosta@gmail.com>
2021-05-02 01:08:35 -0300

    feat(views): adding views to application

---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = (props) => {
+  return (
+    <div>
+      <h1>Página Inicial</h1>
+      <p>Seja bem vindo</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/views/Param.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Param = (props) => {
+  return (
+    <div className="Param">
+      This view receives a param
+    </div>
+  )
+};
+
+export default Param;
